dd0584428eb1f7c564317a32b7f0cec9182aee6e
Kai Hao <kevin830726@gmail.com>
2020-01-25 15:53:39 +0800

    Style the homepage a little bit

--- pages/index.js
+++ pages/index.js
@@ -9,17 +9,91 @@ const Home = () => (
         name="description"
         content="View the Github contributions of a user"
       />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1"
+      ></meta>
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
-    <h1>Contributions</h1>
+    <main>
+      <form
+        role="search"
+        aria-labelledby="label"
+        onSubmit={e => {
+          e.preventDefault();
 
-    <section>
-      <h2>View the Github contributions of </h2>
-      <input />
-    </section>
+          window.location.assign('/' + e.currentTarget.elements.userName.value);
+        }}
+      >
+        <h1>Contributions</h1>
 
-    <style jsx>{``}</style>
+        <h2 id="label">View the Github contributions of</h2>
+
+        <input name="userName" placeholder="kevin940726" />
+      </form>
+    </main>
+
+    <style jsx global>{`
+      html,
+      body {
+        margin: 0;
+        font-family: 'Helvetica';
+        background-color: #24292e;
+      }
+    `}</style>
+
+    <style jsx>{`
+      main {
+        width: 100%;
+        max-width: 600px;
+        margin: auto;
+        padding-top: 40px;
+      }
+
+      h1,
+      h2 {
+        margin-top: 0;
+      }
+
+      h2 {
+        font-size: 18px;
+      }
+
+      form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        height: 186;
+        width: 100%;
+        padding: 16px;
+        font-size: 16px;
+        background-color: #24292e;
+        line-height: 1.5;
+        color: #fff;
+        font-weight: 600;
+        box-sizing: border-box;
+      }
+
+      input {
+        background-color: hsla(0, 0%, 100%, 0.125);
+        border: none;
+        height: 28px;
+        width: 300px;
+        border-radius: 3px;
+        padding: 6px 8px;
+        color: #fff;
+        font-weight: 400;
+        font-size: 14px;
+        box-sizing: border-box;
+        outline: none;
+      }
+
+      input:focus {
+        background-color: #fff;
+        color: #000;
+      }
+    `}</style>
   </>
 );
 
